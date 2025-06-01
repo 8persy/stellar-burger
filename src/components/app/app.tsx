dfe8e88c7d34f524a5aaf4310598c0e10267c9c2
8percy8 <izergin.v05@mail.ru>
2025-06-01 16:14:26 +0300

    fix: after review

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -75,11 +75,11 @@ export const App = () => {
     <div className={styles.app}>
       <AppHeader />
       <Routes location={backgroundLocation || location}>
-        <Route path="*" element={<NotFound404 />} />
-        <Route path="/" element={<ConstructorPage />} />
-        <Route path="/feed" element={<Feed />} />
+        <Route path='*' element={<NotFound404 />} />
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
         <Route
-          path="/login"
+          path='/login'
           element={
             <ProtectedRoute unAuthOnly>
               <Login />
@@ -87,7 +87,7 @@ export const App = () => {
           }
         />
         <Route
-          path="/register"
+          path='/register'
           element={
             <ProtectedRoute unAuthOnly>
               <Register />
@@ -95,7 +95,7 @@ export const App = () => {
           }
         />
         <Route
-          path="/forgot-password"
+          path='/forgot-password'
           element={
             <ProtectedRoute unAuthOnly>
               <ForgotPassword />
@@ -103,7 +103,7 @@ export const App = () => {
           }
         />
         <Route
-          path="/reset-password"
+          path='/reset-password'
           element={
             <ProtectedRoute unAuthOnly>
               <ResetPassword />
@@ -111,7 +111,7 @@ export const App = () => {
           }
         />
         <Route
-          path="/profile"
+          path='/profile'
           element={
             <ProtectedRoute>
               <Profile />
@@ -119,17 +119,17 @@ export const App = () => {
           }
         />
         <Route
-          path="/profile/orders"
+          path='/profile/orders'
           element={
             <ProtectedRoute>
               <ProfileOrders />
             </ProtectedRoute>
           }
         />
-        <Route path="/feed/:number" element={<OrderInfo />} />
-        <Route path="/ingredients/:id" element={<IngredientDetails />} />
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
         <Route
-          path="/profile/orders/:number"
+          path='/profile/orders/:number'
           element={
             <ProtectedRoute>
               <OrderInfo />
@@ -141,7 +141,7 @@ export const App = () => {
       {isModalOpened && backgroundLocation && (
         <Routes>
           <Route
-            path="/ingredients/:id"
+            path='/ingredients/:id'
             element={
               <Modal
                 title={'Описание ингредиента'}
@@ -154,7 +154,7 @@ export const App = () => {
             }
           />
           <Route
-            path="/profile/orders/:number"
+            path='/profile/orders/:number'
             element={
               <ProtectedRoute>
                 <Modal
@@ -169,7 +169,7 @@ export const App = () => {
             }
           />
           <Route
-            path="/feed/:number"
+            path='/feed/:number'
             element={
               <Modal
                 title={'Заказ'}
