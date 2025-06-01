a8a6cf43562e55b8d9c6459db5cef93adeb07ef1
8percy8 <izergin.v05@mail.ru>
2025-06-01 15:26:59 +0300

    fix: tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 require('dotenv').config();
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:4000',
     setupNodeEvents(on, config) {
       config.env = {
         ...process.env,
