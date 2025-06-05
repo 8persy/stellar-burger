2d4ab48560f4359302e23b6f746e25fd5eabe97a
8percy8 <izergin.v05@mail.ru>
2025-06-05 17:27:11 +0300

    fix: linter after review

--- src/components/orders-list/orders-list.tsx
+++ src/components/orders-list/orders-list.tsx
@@ -5,7 +5,8 @@ import { OrdersListUI } from '@ui';
 
 export const OrdersList: FC<OrdersListProps> = memo(({ orders }) => {
   const orderByDate = [...orders].sort(
-    (start, end) => new Date(end.createdAt).getTime() - new Date(start.createdAt).getTime()
+    (start, end) =>
+      new Date(end.createdAt).getTime() - new Date(start.createdAt).getTime()
   );
 
   return <OrdersListUI orderByDate={orderByDate} />;
